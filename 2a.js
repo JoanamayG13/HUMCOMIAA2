4c67216fcdae7aea8be6afde934c8c32fa3145e4
JoanamayG13 <98304250+JoanamayG13@users.noreply.github.com>
2022-04-28 15:28:22 +0800

    Add files via upload

---
+++ 2a.js
@@ -0,0 +1,5 @@
+document.getElementById("1").innerHTML="Joana May M.";
+document.getElementById("2").innerHTML="BS Information Technology";
+document.getElementById("3").innerHTML="1st Year";
+document.getElementById("4").innerHTML="HUMCOM-IAA1";
+document.getElementById("5").innerHTML="Introduction to Website";
