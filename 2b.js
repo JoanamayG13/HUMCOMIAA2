0216b8e06c80d127f77ee3a3ecbefeb4b2442973
JoanamayG13 <98304250+JoanamayG13@users.noreply.github.com>
2022-04-28 15:28:51 +0800

    Add files via upload

---
+++ 2b.js
@@ -0,0 +1,6 @@
+document.write("Joana May Guzmana" + "<br>");
+document.write("BS Information Technology" + "<br>");
+document.write("1st Year" + "<br>");
+document.write("HUMCOM-IAA2" + "<br>");
+document.write("Introduction to Website" + "<br>");
+
